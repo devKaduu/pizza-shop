45261cefb3e902edb7a8ce9128f7f8f93837ad42
Carlos Eduardo <carlos.oliveira@fiap.com.br>
2025-02-07 02:27:42 -0300

    feat: update shadcn/ui

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -4,7 +4,11 @@ const Dashboard = () => {
   return (
     <>
       <Helmet title="Dashboard" />
-      <h1>Dashboard</h1>
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tighter">Dashboard</h1>
+
+        <div className="grid grid-cols-4 gap-4"></div>
+      </div>
     </>
   );
 };
