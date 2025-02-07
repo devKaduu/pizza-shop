e2b6f175560640a00f00bb22a4ea7664ad5997cd
Carlos Eduardo <carlos.oliveira@fiap.com.br>
2025-02-07 02:59:55 -0300

    feat: create cards for the dashboard page

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -1,4 +1,8 @@
 import { Helmet } from "react-helmet-async";
+import { MonthRevenueCard } from "./month-revenue-card";
+import { MonthOrdersAmoutCard } from "./month-orders-amout-card";
+import { DayOrdersAmoutCard } from "./day-orders-amout-card";
+import { MonthCanceledOrdersAmoutCard } from "./month-canceled-orders-amount-card";
 
 const Dashboard = () => {
   return (
@@ -7,7 +11,12 @@ const Dashboard = () => {
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tighter">Dashboard</h1>
 
-        <div className="grid grid-cols-4 gap-4"></div>
+        <div className="grid grid-cols-4 gap-4">
+          <MonthRevenueCard />
+          <MonthOrdersAmoutCard />
+          <DayOrdersAmoutCard />
+          <MonthCanceledOrdersAmoutCard />
+        </div>
       </div>
     </>
   );
